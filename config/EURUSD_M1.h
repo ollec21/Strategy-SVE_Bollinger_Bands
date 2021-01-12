3eb3a3a2b50023336e788165e7e2d7f0ea0edf25
kenorb <kenorb@users.noreply.github.com>
2021-01-12 06:09:27 +0000

    Optimize SignalOpen params (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M1.h
+++ config/EURUSD_M1.h
@@ -1,12 +1,14 @@
-/*
+/**
  * @file
- * Defines strategy's and indicator's default parameter values
- * for the given pair symbol and timeframe.
+ * Defines default strategy parameter values for the given timeframe.
  */
 
 // Defines indicator's parameter values for the given pair symbol and timeframe.
 struct Indi_SVE_Bollinger_Bands_Params_M1 : Indi_SVE_Bollinger_Bands_Params {
   Indi_SVE_Bollinger_Bands_Params_M1() : Indi_SVE_Bollinger_Bands_Params(indi_svebbands_defaults, PERIOD_M1) {
+    applied_price = (ENUM_APPLIED_PRICE)0;
+    ma_method = (ENUM_MA_METHOD)0;
+    period = 0;
     shift = 0;
   }
 } indi_svebbands_m1;
@@ -16,14 +18,14 @@ struct Stg_SVE_Bollinger_Bands_Params_M1 : StgParams {
   // Struct constructor.
   Stg_SVE_Bollinger_Bands_Params_M1() : StgParams(stg_svebbands_defaults) {
     lot_size = 0;
-    signal_open_method = 0;
-    signal_open_filter = 1;
-    signal_open_level = (float)0;
-    signal_open_boost = 0;
+    signal_open_method = -4;
+    signal_open_filter = 0;
+    signal_open_level = (float)0.0;
+    signal_open_boost = 1;
     signal_close_method = 0;
-    signal_close_level = (float)0;
+    signal_close_level = (float)0.0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)0.0;
     tick_filter_method = 1;
     max_spread = 0;
   }
